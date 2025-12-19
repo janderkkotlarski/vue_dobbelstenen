49480608128b0c8343c17504c156bce5858345a4
unknown <j.d.kotlarski@gmx.com>
2025-12-19 15:57:30 +0100

    keep checking simple out of scope variable usage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <script setup>
-import {ref} from 'vue';
 import Display from './components/Display.vue';
-import Eye from './components/Eye.vue';
-import Line from './components/Line.vue';
-import Dice from './components/Dice.vue';
-
-const valueMax = 6;
-const valueFeedback = ref(0);
-
-const changer = () => {
-    if (valueFeedback.value < valueMax) {
-        ++valueFeedback.value;
-    } else {
-        valueFeedback.value = 0;
-    }
-};
-
-/*
-
-<Dice @click="changer" v-model:value="valueFeedback" />
-
-<Dice @click="changer" v-model:value="valueFeedback" />
-*/
 </script>
 
 <template>
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -2,9 +2,11 @@
 import {ref} from 'vue';
 import Dice from './Dice.vue';
 
+const valueMax = 6;
+
 const diceAmount = 8;
 
-const roll = () => Math.floor(6 * Math.random()) + 1;
+const roll = () => Math.floor(valueMax * Math.random()) + 1;
 
 const rolling = () => {
     let values = [];
@@ -28,12 +30,55 @@ const rerolling = () => {
         reroll(index);
     }
 };
+
+const valueCountInit = () => {
+    let counts = [];
+
+    for (let i = 0; i < valueMax; ++i) {
+        counts.push({diceValue: i + 1, rollCount: 0});
+    }
+
+    return counts;
+};
+
+// const valueCounts = ref(valueCountInit());
+
+const valueRecount = () => {
+    let counts = [];
+
+    for (let i = 0; i < valueMax; ++i) {
+        let count = 0;
+
+        for (let j = 0; j < diceAmount; ++j) {
+            if (diceValues.value[j] === i + 1) {
+                ++count;
+            }
+        }
+
+        counts.push({diceValue: i + 1, rollCount: count});
+    }
+
+    return counts;
+};
+
+const valueCounts = ref(valueRecount());
 </script>
 
 <template>
-    <div class="row">
+    <div>
         <Dice @click="reroll(index - 1)" v-for="index in diceAmount" v-model:value="diceValues[index - 1]" />
     </div>
-
+    <br />
     <button @click="rerolling">Opnieuw rollen?</button>
+    <br />
+    <table>
+        <tr>
+            <th>Waarde</th>
+            <th>Hoeveelheid</th>
+        </tr>
+        <tr v-for="value in valueCounts">
+            <td>{{ value.diceValue }}</td>
+            <td>{{ value.rollCount }}</td>
+        </tr>
+    </table>
 </template>
