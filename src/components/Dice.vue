a1098e949d3962cc4be2e464e865a3d68c252062
unknown <j.d.kotlarski@gmx.com>
2026-01-13 15:54:39 +0100

    v-for needs :key in order to meet coding standards

--- src/components/Dice.vue
+++ src/components/Dice.vue
@@ -5,19 +5,27 @@ import Line from './Line.vue';
 const value = defineModel('value', {type: Number, default: 0});
 
 // Possible dice line configs
+// const diceLines = [
+//     {id: 0, lines: [0, 0, 0]},
+//     {lines: [0, 2, 0]}, // 1
+//     {lines: [1, 0, 3]}, // 2
+//     {lines: [1, 2, 3]}, // 3
+//     {lines: [4, 0, 4]}, // 4
+//     {lines: [4, 2, 4]}, // 5
+//     {lines: [4, 4, 4]}, // 6
+// ];
+
 const diceLines = [
-    [0, 0, 0], // 0
-    [0, 2, 0], // 1
-    [1, 0, 3], // 2
-    [1, 2, 3], // 3
-    [4, 0, 4], // 4
-    [4, 2, 4], // 5
-    [4, 4, 4], // 6
+    {id: 1, lines: [0, 0, 1, 1, 4, 4, 4]},
+    {id: 2, lines: [0, 2, 0, 2, 0, 2, 4]},
+    {id: 3, lines: [0, 0, 3, 3, 4, 4, 4]},
 ];
+
+// <Line v-for="diceLine in diceLines" :key="diceLine.id" :selected="diceLine.lines[value]" />
 </script>
 
 <template>
     <div class="dice">
-        <Line v-for="i in 3" :selected="diceLines[value][i - 1]" />
+        <Line v-for="diceLine in diceLines" :key="diceLine.id" :selected="diceLine.lines[value]" />
     </div>
 </template>
