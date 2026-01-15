d1bbd62afdd0c8ce29af00de575e9dae419fc24f
unknown <j.d.kotlarski@gmx.com>
2026-01-15 14:02:15 +0100

    v-for necessitates a unique key according to vue coding standards, so implemented arrays needed to be reworked to enable this

--- src/components/Dice.vue
+++ src/components/Dice.vue
@@ -4,24 +4,12 @@ import Line from './Line.vue';
 // Default is for empty die
 const value = defineModel('value', {type: Number, default: 0});
 
-// Possible dice line configs
-// const diceLines = [
-//     {id: 0, lines: [0, 0, 0]},
-//     {lines: [0, 2, 0]}, // 1
-//     {lines: [1, 0, 3]}, // 2
-//     {lines: [1, 2, 3]}, // 3
-//     {lines: [4, 0, 4]}, // 4
-//     {lines: [4, 2, 4]}, // 5
-//     {lines: [4, 4, 4]}, // 6
-// ];
-
+// Most effective array config for v-for coding standards
 const diceLines = [
     {id: 1, lines: [0, 0, 1, 1, 4, 4, 4]},
     {id: 2, lines: [0, 2, 0, 2, 0, 2, 4]},
     {id: 3, lines: [0, 0, 3, 3, 4, 4, 4]},
 ];
-
-// <Line v-for="diceLine in diceLines" :key="diceLine.id" :selected="diceLine.lines[value]" />
 </script>
 
 <template>
--- src/components/Line.vue
+++ src/components/Line.vue
@@ -5,16 +5,14 @@ import Eye from './Eye.vue';
 const selected = defineModel('selected', {type: Number, default: 0});
 
 const eyeLines = [
-    ['invis', 'invis', 'invis'],
-    ['visible', 'invis', 'invis'],
-    ['invis', 'visible', 'invis'],
-    ['invis', 'invis', 'visible'],
-    ['visible', 'invis', 'visible'],
+    {id: 1, eyes: ['_______', 'visible', '_______', '_______', 'visible']},
+    {id: 2, eyes: ['_______', '_______', 'visible', '_______', '_______']},
+    {id: 3, eyes: ['_______', '_______', '_______', 'visible', 'visible']},
 ];
 </script>
 
 <template>
     <div>
-        <Eye v-for="i in 3" :visibility="eyeLines[selected][i - 1]" />
+        <Eye v-for="eyeLine in eyeLines" :key="eyeLine.id" :visibility="eyeLine.eyes[selected]" />
     </div>
 </template>
